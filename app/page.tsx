3b6a3e637b40d17efb352e9226de159bdbd4d569
atiq ur Rehman <atiqu555@gmail.com>
2023-04-16 23:04:34 +0500

    code-cleaning

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,6 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <Link href={"/about"}>About</Link>
-      <Link href={"/blog"}>Blog</Link>
     </main>
   );
 }
